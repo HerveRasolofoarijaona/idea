94290b3c4374247a3763297f4b7acc07ec5d9835
Herve <herve.rasolofoarijaona@esti.mg>
2019-05-22 18:27:56 +0300

    Connect button for a version beta

--- app/src/main/java/lova/idea/MainActivity.java
+++ app/src/main/java/lova/idea/MainActivity.java
@@ -1,9 +1,11 @@
 package lova.idea;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
@@ -12,21 +14,21 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button login = findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View actuelView)    //au clic sur le bouton
+            {
+                Intent intent = new Intent(MainActivity.this, index.class);  //Lancer l'activité index
+                startActivity(intent);    //Afficher la vue
+            }
+        });
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
+        @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
